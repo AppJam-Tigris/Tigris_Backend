27e25853edc1f22720b8e3c40563afba0e90b23d
김범진 <sdpthf@naver.com>
2021-12-17 20:06:23 +0900

    ⚙ :: Init Project

---
+++ tigris_server/src/main/java/team/appjam/tigris_server/TigrisServerApplication.java
@@ -0,0 +1,13 @@
+package team.appjam.tigris_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigrisServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TigrisServerApplication.class, args);
+    }
+
+}
---
+++ tigris_server/src/test/java/team/appjam/tigris_server/TigrisServerApplicationTests.java
@@ -0,0 +1,13 @@
+package team.appjam.tigris_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigrisServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
