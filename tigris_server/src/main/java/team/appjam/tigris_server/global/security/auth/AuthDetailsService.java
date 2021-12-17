876649f111e6b8b0bfe11df1160e7e5b9fc920ec
김범진 <sdpthf@naver.com>
2021-12-17 21:02:50 +0900

    📑 :: AuthDetails

---
+++ tigris_server/src/main/java/team/appjam/tigris_server/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,24 @@
+package team.appjam.tigris_server.global.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import team.appjam.tigris_server.domain.user.repository.UserRepository;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String uid) throws UsernameNotFoundException {
+        return new AuthDetails(
+                userRepository.findByUid(uid)
+                        .orElseThrow()
+        );
+    }
+
+}
