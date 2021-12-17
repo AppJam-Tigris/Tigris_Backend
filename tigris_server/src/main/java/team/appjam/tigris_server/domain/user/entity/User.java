f4d01548d62c5a491dcd3ee6ff40399b672ae7db
김범진 <sdpthf@naver.com>
2021-12-17 21:03:42 +0900

    📑 :: User

---
+++ tigris_server/src/main/java/team/appjam/tigris_server/domain/user/entity/User.java
@@ -0,0 +1,24 @@
+package team.appjam.tigris_server.domain.user.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String uid;
+
+    private String password;
+
+}
