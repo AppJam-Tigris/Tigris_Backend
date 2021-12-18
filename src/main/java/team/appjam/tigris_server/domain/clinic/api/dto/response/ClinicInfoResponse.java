6a08b64dc296fdde0c4d40fc92a94bebc41b13eb
김범진 <sdpthf@naver.com>
2021-12-19 07:38:32 +0900

    ⚡ :: ClinicId 추가

--- src/main/java/team/appjam/tigris_server/domain/clinic/api/dto/response/ClinicInfoResponse.java
+++ src/main/java/team/appjam/tigris_server/domain/clinic/api/dto/response/ClinicInfoResponse.java
@@ -8,12 +8,14 @@ import team.appjam.tigris_server.domain.clinic.entity.Clinic;
 @AllArgsConstructor
 public class ClinicInfoResponse {
 
+    private final Integer clinicId;
     private final String name;
     private final String address;
     private final String city;
     private final String managerPhoneNumber;
 
     public ClinicInfoResponse(Clinic clinic) {
+        this.clinicId = clinic.getId();
         this.name = clinic.getName();
         this.address = clinic.getAddress();
         this.city = clinic.getCity();
