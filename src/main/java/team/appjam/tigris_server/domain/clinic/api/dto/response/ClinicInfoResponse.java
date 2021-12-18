eba069131289e5767b77b91178317946f416e44e
김범진 <sdpthf@naver.com>
2021-12-19 07:44:03 +0900

    🐛 :: 값 바뀜

--- src/main/java/team/appjam/tigris_server/domain/clinic/api/dto/response/ClinicInfoResponse.java
+++ src/main/java/team/appjam/tigris_server/domain/clinic/api/dto/response/ClinicInfoResponse.java
@@ -19,7 +19,7 @@ public class ClinicInfoResponse {
         this.name = clinic.getName();
         this.address = clinic.getAddress();
         this.city = clinic.getCity();
-        this.managerPhoneNumber = clinic.getManagerPhoneNumber();
+        this.managerPhoneNumber = clinic.getManagerClinic();
     }
 
 }
